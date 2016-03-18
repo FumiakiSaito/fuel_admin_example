611a754a7fc7db2f0e36e540e22b6e5f40362926
fumiaki <haratairani3000points@gmail.com>
2016-03-18 11:53:49 +0900

    社員テーブル、部署テーブル追加に伴う修正
    - テーブル作成クエリ修正
    - データ初期化クエリ修正

--- db/initval.sql
+++ db/initval.sql
@@ -5,3 +5,10 @@
 -- 管理者登録
 -- admin / admin
 INSERT INTO admin (id, username, password, `group`, email, last_login, created_at, updated_at) VALUES(1, 'admin', 'K0VTGAnXgKh1OJcRXlmDcf64j5wriR1MuZdREx1f1lA=', 100, 'admin@hoge.com', UNIX_TIMESTAMP(NOW()), UNIX_TIMESTAMP(NOW()) , UNIX_TIMESTAMP(NOW()));
+
+
+-- 部署登録
+INSERT INTO division(id, name, created_at, updated_at) VALUES 
+(1, '開発部', NOW(), NOW()),
+(2, '総務部', NOW(), NOW());
+
